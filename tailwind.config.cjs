4aa6beb571e83f9871d20c97ce61b1d5d222759c
DucHoang-UIT <hoangpd@unicloud.com.vn>
2023-04-26 18:43:26 +0700

    test commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+
+module.exports = {
+    content: ['./src/**/*.{jx,jsx,ts,tsx}', './public/index.html'],
+    theme:{
+        extend:{
+            colors:{
+                primary: '#1B73E8',
+            }
+        }
+    },
+    plugin:[],
+}
